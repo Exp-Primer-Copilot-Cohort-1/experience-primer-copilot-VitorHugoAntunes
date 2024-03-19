2015bcf53cfa223839105243ebd0084077ea4533
Vitor Hugo Antunes Passos <vitor4ntunes@gmail.com>
2024-03-19 17:25:42 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server for comment
+const server = http.createServer((req, res) => {
+    // Handle incoming requests here
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
